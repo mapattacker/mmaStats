236b4df587a8164c6f47c3030a8a8703d87a3a70
Jake Teo <teo.siyang@gmail.com>
2016-04-10 10:42:40 +0800

    Added files via upload

---
+++ mmaCreate.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import urllib, sqlite3, time, traceback
+
+######### CREATE SQLITE TABLE ############
+conn = sqlite3.connect('MMA.sqlite')
+cur = conn.cursor()
+
+cur.executescript('''
+DROP TABLE IF EXISTS fighter;
+DROP TABLE IF EXISTS relation;
+DROP TABLE IF EXISTS html;
+DROP TABLE IF EXISTS nationality;
+DROP TABLE IF EXISTS weightClass;
+
+CREATE TABLE fighter (
+    id              INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    url             TEXT UNIQUE,
+    scanned         TEXT,
+    name            TEXT,
+    count_new       INTEGER,
+    birthday        TEXT,
+    weightClass_id  INTEGER,
+    country_id      INTEGER,
+    First_Fight     TEXT,
+    Last_Fight      TEXT,
+    totalFights     INTEGER,
+    totalWins       INTEGER,
+    totalLosses     INTEGER,
+    totalDraws      INTEGER,
+    totalNC         INTEGER
+);
+
+CREATE TABLE relation (
+    from_id          INTEGER,
+    to_id            INTEGER
+);
+
+CREATE TABLE html (
+    id              INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    body            TEXT
+);
+
+CREATE TABLE nationality (
+    id              INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    country         TEXT UNIQUE
+);
+
+CREATE TABLE weightClass (
+    id              INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    weight          TEXT UNIQUE
+)
+''')
+
+print 'Database and tables Created'
